6c1f9df0f4958240719a0d5f7dace55af0e22665
MattRuetz <matt.b.ruetz@gmail.com>
2022-02-24 12:12:14 -0500

    fix mouse tracking to move the paddle to edges of screen with any window size. Game over screen shows after a score of 7 is reached.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
 const width = 500;
 const height = 700;
-const screenWidth = window.screen.width;
+const screenWidth = window.innerWidth;
 const canvasPosition = screenWidth / 2 - width / 2;
 const isMobile = window.matchMedia('(max-width: 600px)');
 const gameOverEl = document.createElement('div');
@@ -44,8 +44,8 @@ if (isMobile.matches) {
 let playerScore = 0;
 let computerScore = 0;
 const winningScore = 7;
-// let isGameOver = true;
-// let isNewGame = true;
+let isGameOver = false;
+let isNewGame = true;
 
 // Render Everything on Canvas
 function renderCanvas() {
@@ -172,27 +172,30 @@ function computerAI() {
 
 function showGameOverEl(winner) {
     // Hide Canvas
-    // // Container
-    // gameOverEl.textContent = '';
-    // gameOverEl.classList.add('game-over-container');
-    // // Title
-    // const title = document.createElement('h1');
-    // title.textContent = `${winner} Wins!`;
-    // // Button
-    // const playAgainBtn = document.createElement('button');
-    // playAgainBtn.setAttribute('onclick', 'startGame()');
-    // playAgainBtn.textContent = 'Play Again';
-    // // Append
+    canvas.hidden = true;
+    // Container
+    gameOverEl.textContent = '';
+    gameOverEl.classList.add('game-over-container');
+    // Title
+    const title = document.createElement('h1');
+    title.textContent = `${winner} Wins!`;
+    // Button
+    const playAgainBtn = document.createElement('button');
+    playAgainBtn.setAttribute('onclick', 'startGame()');
+    playAgainBtn.textContent = 'Play Again';
+    // Append
+    gameOverEl.append(title, playAgainBtn);
+    body.appendChild(gameOverEl);
 }
 
 // Check If One Player Has Winning Score, If They Do, End Game
 function gameOver() {
-    // if (playerScore === winningScore || computerScore === winningScore) {
-    //   isGameOver = ;
-    //   // Set Winner
-    //   let winner = ;
-    //   showGameOverEl(winner);
-    // }
+    if (playerScore === winningScore || computerScore === winningScore) {
+        isGameOver = true;
+        // Set Winner
+        let winner = playerScore > computerScore ? 'Player 1' : 'CPU';
+        showGameOverEl(winner);
+    }
 }
 
 // Called Every Frame
@@ -201,16 +204,21 @@ function animate() {
     ballMove();
     ballBoundaries();
     computerAI();
-    window.requestAnimationFrame(animate); // recursive game loop
+    gameOver(); // Checks if game has been won
+
+    if (!isGameOver) {
+        window.requestAnimationFrame(animate); // recursive game loop
+    }
 }
 
 // Start Game, Reset Everything
 function startGame() {
-    // if (isGameOver && !isNewGame) {
-
-    // }
-    // isGameOver = false;
-    // isNewGame = true;
+    if (isGameOver && !isNewGame) {
+        body.removeChild(gameOverEl);
+        canvas.hidden = false;
+    }
+    isGameOver = false;
+    isNewGame = false;
     playerScore = 0;
     computerScore = 0;
     ballReset();
